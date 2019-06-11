88802e21c89283b85aa12c0fcb5ae9ee18d090e4
Marsel Sidikov <sidikov.marsel@gmail.com>
2019-06-11 16:26:31 +0300

    add reader and writer

--- Projects/# 23. SimpleService/src/ru/itis/repositories/UsersRepository.java
+++ Projects/# 23. SimpleService/src/ru/itis/repositories/UsersRepository.java
@@ -1,10 +1,9 @@
 package ru.itis.repositories;
 
 import ru.itis.models.User;
+import ru.itis.models.UserRole;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
 public class UsersRepository {
     private String fileName;
@@ -28,6 +27,63 @@ public class UsersRepository {
     }
 
     public User[] findAll() {
-        return null;
+        // Создаем массив для результата
+        // сюда положим пользователей
+        User users[] = new User[100];
+        try {
+            // открываем файл для чтения
+            Reader reader = new FileReader(fileName);
+            // открываем буфер для чтения из файла и передаем
+            // туда файл
+            BufferedReader bufferedReader
+                    = new BufferedReader(reader);
+
+            // считываем из файла первую строку
+            String currentUserLine = bufferedReader.readLine();
+            // начинаем с нуля
+            int currentIndex = 0;
+            // пока файл не кончился
+            while (currentUserLine != null) {
+                // разбиваем исходную строку
+                // на элементы по разелителю |
+                String userData[] = currentUserLine.split("\\|");
+                String idString = userData[0];
+                String firstName = userData[1];
+                String lastName = userData[2];
+                String ageAsString = userData[3];
+                String phone = userData[4];
+                String email = userData[5];
+                String login = userData[6];
+                String password = userData[7];
+                UserRole role = UserRole.valueOf(userData[8]);
+
+                User user = User.builder()
+                        .login(login)
+                        .password(password)
+                        .build();
+
+                if (!firstName.equals("null")) {
+                    user.setFirstName(firstName);
+                }
+
+                if (!lastName.equals("null")) {
+                    user.setLastName(lastName);
+                }
+
+                if (!ageAsString.equals("null")) {
+                    user.setAge(Integer.parseInt(ageAsString));
+                }
+
+                user.setRole(role);
+
+                users[currentIndex] = user;
+                currentIndex++;
+                currentUserLine = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException();
+        }
+
+        return users;
     }
 }
--- Projects/# 23. SimpleService/src/ru/itis/tests/MainUsersServiceTest.java
+++ Projects/# 23. SimpleService/src/ru/itis/tests/MainUsersServiceTest.java
@@ -6,13 +6,18 @@ import ru.itis.repositories.IdGenerator;
 import ru.itis.repositories.UsersRepository;
 import ru.itis.services.UsersService;
 
+import java.util.Arrays;
+
 public class MainUsersServiceTest {
     public static void main(String[] args) {
         IdGenerator idGenerator = new IdGenerator("users_id.txt");
         UsersRepository usersRepository = new UsersRepository("users.txt", idGenerator);
         UsersService service = new UsersService(usersRepository);
-        SignUpForm form = new SignUpForm("marsel", "qwerty007");
-        User user = service.signUp(form);
-        System.out.println(user);
+//        SignUpForm form = new SignUpForm("marsel", "qwerty007");
+//        User user = service.signUp(form);
+//        System.out.println(user);
+
+        User users[] = usersRepository.findAll();
+        System.out.println(Arrays.toString(users));
     }
 }
